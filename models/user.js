76fb3932122d771cd9dbbab5c1ad41c07a93a7e4
Ahmet Kutay Karacair <akkaracair@gmail.com>
2020-11-22 17:29:32 +0300

    login test 5

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const confiq = require('../config/config').get(process.env.NODE_ENV);
 const salt = 10;
 
