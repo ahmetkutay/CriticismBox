0fd8baf0cc6cce95daeaf6a1662553c90e31a0a0
Ahmet Kutay Karacair <akkaracair@gmail.com>
2020-11-22 19:11:21 +0300

    test 15

--- models/user.js
+++ models/user.js
@@ -57,7 +57,7 @@ userScheme.methods.comparepassword = function (password, cb) {
 userScheme.methods.generateToken = function (cb) {
     var user = this;
     var token = jwt.sign(user._id.toHexString(), confiq.SECRET);
-
+    
     user.token = token;
     user.save(function (err, user) {
         if (err) return cb(err);
